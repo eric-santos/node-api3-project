2ad3600c86d8d9ce41ec37e55e50ce9f9501399f
eric <eric355402@yahoo.com>
2020-03-17 21:14:44 -0500

    added cors()

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const postsRouter = require('./posts/postRouter');
-const userRouter = require('./users/userRouter');
+const express = require("express");
+const postsRouter = require("./posts/postRouter");
+const userRouter = require("./users/userRouter");
 
 const server = express();
 
 //global middleware section
 server.use(express.json());
+server.use(cors());
 server.use(logger);
-server.use('/posts', postsRouter);
-server.use('/users', userRouter);
+server.use("/posts", postsRouter);
+server.use("/users", userRouter);
 
-server.get('/', (req, res) => {
-  res.send('hello world!');
+server.get("/", (req, res) => {
+  res.send("hello world!");
 });
 
 function logger(req, res, next) {
